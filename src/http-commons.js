5d967d02e38a3b16b14cd671b32f9cbffa8127fa
cloudmonsterrepo <contacto@cloudmosnter.co>
2020-12-17 16:39:38 -0500

    fix Back-End

--- src/http-commons.js
+++ src/http-commons.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://drivetic.herokuapp.com/",
+    baseURL: "https://ticdrive.herokuapp.com/",
     headers: {
         "Content-type": "application/json",
         "Access-Control-Allow-Origin": "*",
