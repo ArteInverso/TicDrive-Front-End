350daa0bd0b9af07f28d1e2d087b72c53e223884
cloudmonsterrepo <contacto@cloudmosnter.co>
2020-12-17 14:13:49 -0500

    Version 0.1.0

--- src/services/CargasArchivosService.js
+++ src/services/CargasArchivosService.js
@@ -2,15 +2,21 @@ import http from "../http-commons";
 
 class UploadFilesService {
     upload(form, onUploadProgress) {
-        let formData = new FormData();
-
-        formData.append("uploaded_file", form.uploaded_file);
+        let uploaded_file = new FormData();
+        const util = require('util')
+        uploaded_file.append("uploaded_file", form.uploaded_file[0]);
         let iddoc = form.iddoc;
         let fecvencimientodoc = form.fecvencimientodoc;
         let nomdoc = form.nomdoc;
         let idusuario = form.idusuario;
+        console.log(util.inspect(uploaded_file, false, null, true /* enable colors */ ))
+        console.log(uploaded_file)
+        console.log(form.uploaded_file)
+        console.log(util.inspect(form.uploaded_file, false, null, true /* enable colors */ ))
+        typeof(uploaded_file)
+        typeof(form.uploaded_file)
 
-        return http.post("/upload-file", formData, {
+        return http.post("/upload-file", uploaded_file, {
                 params: {
                     iddoc,
                     fecvencimientodoc,
